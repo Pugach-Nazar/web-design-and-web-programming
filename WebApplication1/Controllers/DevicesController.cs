4d6904bf8a04adf5f28acc1e0175c9fff638bff4
Pugach-Nazar <qwertyasd0933@gmail.com>
2023-11-03 15:39:04 +0200

    03.11.2023

--- WebApplication1/Controllers/DevicesController.cs
+++ WebApplication1/Controllers/DevicesController.cs
@@ -54,6 +54,7 @@ namespace WebApplication1.Controllers
         // POST: Devices/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Price,Color")] Device device)
